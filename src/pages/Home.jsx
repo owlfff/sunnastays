13280a3cf2ff24e5cc765dbb84435408aa62e99b
Oliver Flint <oliverflint@MacBook-Pro.local>
2026-03-27 12:06:32 +0000

    Fix searchStays import on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { searchStays } from '../api';
 import { useNavigate } from 'react-router-dom';
 import StayCard from '../components/StayCard';
 import Footer from '../components/Footer';
