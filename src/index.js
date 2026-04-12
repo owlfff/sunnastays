f17cd8ebe70d59cf0b5fa76bfd468de7711987e1
Oliver Flint <oliverflint@MacBook-Pro.local>
2026-04-12 14:56:15 +0100

    Add PWA support - manifest, icons, service worker

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW registered:', reg.scope))
+      .catch(err => console.log('SW registration failed:', err));
+  });
+}
